acec5e285b4b8688cfee28259274986dc173acaa
Geoff Corvera <geoffcorvera@gmail.com>
2021-02-22 10:23:28 -0800

    connect to db

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
 import http from 'http'
+const { Client }  = require('pg')
+
+const connectionString = 'postgresql://w21wdb5:@1Samsung@dbclass.cs.pdx.edu:5432/w21wdb5'
+
+const client = new Client({ connectionString })
+client.connect((err: any) => {
+    if (err) {
+        console.log('Connection error', err.stack)
+    } else {
+        console.log('Connected')
+    }
+})
+
+const queryStr = "SELECT * FROM spy.agent WHERE first like 'W%'"
+client.query(queryStr, (err: any, res: any) => {
+    console.log(err, res)
+    client.end()
+})
 
 let server = http.createServer((req, res) => {
     // Routes
