a019c46c99e10868704c9e565dcbca257deb6059
Geoff Corvera <geoffcorvera@gmail.com>
2021-02-22 11:11:16 -0800

    move query to /agent route and return first row result as JSON

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import http from 'http'
+import { QueryResult } from 'pg'
 const { Client }  = require('pg')
 
 const connectionString = 'postgresql://w21wdb5:@1Samsung@dbclass.cs.pdx.edu:5432/w21wdb5'
-
 const client = new Client({ connectionString })
 client.connect((err: any) => {
     if (err) {
@@ -12,11 +12,7 @@ client.connect((err: any) => {
     }
 })
 
-const queryStr = "SELECT * FROM spy.agent WHERE first like 'W%'"
-client.query(queryStr, (err: any, res: any) => {
-    console.log(err, res)
-    client.end()
-})
+// TODO: npm install pg-cursor
 
 let server = http.createServer((req, res) => {
     // Routes
@@ -24,8 +20,19 @@ let server = http.createServer((req, res) => {
         res.writeHead(200)
         res.write('<html><body><p>Hello world</p></body></html>')
         res.end()
-    }
-    else {
+    } else if (req.url == '/agent') {
+        client
+            .query("SELECT * FROM spy.agent WHERE first like 'W%'")
+            .then((queryResult: QueryResult) => {
+                console.log(queryResult.rows[0])
+
+                res.writeHead(200, { 'Content-Type': 'application/json' })
+                res.write(JSON.stringify({ results: queryResult.rows[0] }))
+                res.end()
+            })
+            .catch((e: any) => console.log(e.stack))
+
+    } else {
         res.end('Invalid request')
     }
 })
