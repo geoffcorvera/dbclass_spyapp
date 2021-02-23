16cf83e69c96203e1c07b1d61f27746acf6bd81e
Geoffrey Corvera <geoffcorvera@gmail.com>
2021-02-23 01:37:30 -0800

    Display query results to table

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import http from 'http'
-import { QueryResult } from 'pg'
+import { Query, QueryResult } from 'pg'
 const { Client }  = require('pg')
 
 // Don't commit you password
-const pw: String = 'notmypassword'
-const connectionString = `postgresql://w21wdb5:${pw}@dbclass.cs.pdx.edu:5432/w21wdb5`
+const PW = 'foobar'
+const USERNAME = 'w21wdb5'
+const DB = USERNAME
+const HOSTNAME = 'dbclass.cs.pdx.edu'
+const DB_PORT = '5432'
+
+const connectionString = `postgresql://${USERNAME}:${PW}@${HOSTNAME}:${DB_PORT}/${DB}`
 const client = new Client({ connectionString })
 client.connect((err: any) => {
     if (err) {
@@ -14,28 +19,62 @@ client.connect((err: any) => {
     }
 })
 
-// TODO: npm install pg-cursor
-
+// TODO: Should I install pg-cursor?
 let server = http.createServer((req, res) => {
     // Routes
     if (req.url == '/') {
-        res.writeHead(200)
-        res.write('<html><body><p>Hello world</p></body></html>')
-        res.end()
+        sendLandingPage(res)
     } else if (req.url == '/agent') {
         client
             .query("SELECT * FROM spy.agent WHERE last like 'W%'")
-            .then((queryResult: QueryResult) => {
-                res.writeHead(200, { 'Content-Type': 'application/json' })
-                res.write(JSON.stringify({ results: queryResult.rows }))
+            .then((qr: QueryResult) => {
+                res.writeHead(200, { 'content-type': 'text/html' })
+                writeTop(res)
+                writeBody(res, qr.rows)
+                res.write('</html>')
                 res.end()
             })
             .catch((e: any) => console.log(e.stack))
-
     } else {
         res.end('Invalid request')
     }
 })
+
+let sendLandingPage = (res:any): void => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<html><body><p>Spy Database</p></body></html>')
+    res.end()
+}
+
+let writeTop = (res:any): void => {
+    let styling = `
+    <style>
+        table { border-collapse:collapse; width:100%; }
+        table, th, td { border: 1px solid black; }
+        th, td { padding: 10px; }
+    </style>
+    `
+    res.write(
+        '<html><head><title>Spy App</title>' +
+        styling +
+        '</head>'
+    )
+}
+
+let writeBody = (res: any, rows: any): void => {
+    let tableHeader = '<tr>'
+        + '<th>agent_id</th>'
+        + '<th>first</th><th>middle</th><th>last</th>'
+        + '<th>address</th><th>city</th><th>country</th>'
+        + '<th>salary</th>'
+        + '<th>clearance_id</th></tr>'
+
+    let rowsMarkup = rows.map((row:any) => {
+        return '<tr><td>' + Object.values(row).join('</td><td>') + '</td></tr>'
+    })
+    
+    res.write('<body><h1>Agents</h1><table>' + tableHeader + rowsMarkup.join('') + '</table></body>')
+}
 
 const PORT = 8080
 server.listen(PORT)
